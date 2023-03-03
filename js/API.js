8e344153b1e58e1b5606727936dbf9b6b02deaea
David <martinmart515@hotmail.com>
2023-03-02 23:17:32 -0500

    Se implementa correctamente la API

--- js/API.js
+++ js/API.js
@@ -1,15 +1,16 @@
 function mostrarImagenesAPI() {
     
-    var API_KEY = '34087519-a65a6e2dd31e2d2e7aaf69be9';
-    var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('carros');
+    const apiKey = '34087519-a65a6e2dd31e2d2e7aaf69be9';
+    const endpoint = `https://pixabay.com/api/?key=${apiKey}&q=classic cars`;
   
     fetch(endpoint)
       .then(response => response.json())
       .then(data => {
-        data.hits.forEach(image => {
-          const img = document.createElement('img');
-          img.src = image.webformatURL;
-          document.body.appendChild(img);
+        const gridContainer = document.querySelector('.image-grid');
+      data.hits.forEach(image => {
+        const img = document.createElement('img');
+        img.src = image.webformatURL;
+        gridContainer.appendChild(img);
         });
       })
       .catch(error => console.error(error));
